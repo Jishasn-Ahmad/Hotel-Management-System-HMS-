6c23c9d315502c4c2df2fa6bd9850f0c25da2454
Jishan Ahmad <139096572+Jishasn-Ahmad@users.noreply.github.com>
2024-10-29 08:03:16 +0530

    Add files via upload

--- HotelManagementSystem.java
+++ HotelManagementSystem.java
@@ -1,10 +1,8 @@
-
 package hotel.management.system;
 
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
-
 
 public class HotelManagementSystem extends JFrame implements ActionListener{
 
@@ -13,19 +11,19 @@ public class HotelManagementSystem extends JFrame implements ActionListener{
         setLocation(100,100);
         setBounds(100,100,1366,565);
         setLayout(null);
-
+        
         ImageIcon i1 = new ImageIcon(ClassLoader.getSystemResource("icons/first.jpg"));
         JLabel image = new JLabel(i1);
         image.setBounds(0,0,1366,565); // locationx, locationy
         add(image);
-
+        
         JLabel text = new JLabel("HOTEL MANAGEMENT SYSTEM");
         text.setBounds(20,430,1000,90);
         text.setForeground(Color.WHITE);
         text.setFont(new Font("serit",Font.PLAIN, 50));
         image.add(text);
-
-
+        
+        
         JButton next = new JButton("Next");
         next.setBounds(1150,450,150,50);
         next.setBackground(Color.WHITE);
@@ -34,32 +32,33 @@ public class HotelManagementSystem extends JFrame implements ActionListener{
         next.setFont(new Font("serit",Font.PLAIN, 24));
 
         image.add(next);
-
+        
         setVisible(true);
-
+        
         while(true){
             text.setVisible(false);
-            try {
-                Thread.sleep(500);
-            }catch (Exception e){
-                e.printStackTrace();
-            }
-            text.setVisible(true);
-
-            try {
-                Thread.sleep(500);
-            }catch (Exception e){
-                e.printStackTrace();
-            }
-        }
+          try {
+              Thread.sleep(500);
+          }catch (Exception e){
+              e.printStackTrace();
+          }
+          text.setVisible(true);
+          
+          try {
+              Thread.sleep(500);
+          }catch (Exception e){
+              e.printStackTrace();
+          }
     }
-
+    }
+    
     public void actionPerformed(ActionEvent ae){
         setVisible(false);
         new Login();
     }
-
+    
     public static void main(String[] args) {
-        new HotelManagementSystem();
+       new HotelManagementSystem();
     }
+    
 }
